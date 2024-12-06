bc74ce338f7180cb960a7970498601be89928084
MauroGabrielRussoLage <mauro.russo@sofka.com.co>
2024-12-06 14:57:37 -0300

    Se ajusta explicación de pilares

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 
 
 /* Pilares implementados:
-    - Polimorfismo: En el uso de la referencia a la clase abstracta de paciente pero instanciando su implementación
-    - Herencia: En los metodos y atributos originales de la clase abstracta de paciente heredadas por sus extenciones
-    - Encapsulamiento: La protección de los atributos de los objetos mediante getters y setters
-    - Abstracción: Mediante el llamado del metodo toString, que la clase main no sabe que hace internamente, pero si el resultado
+    - Polimorfismo: Se implementa al usar una referencia de la clase abstracta `Paciente`, pero instanciando objetos de sus clases concretas (por ejemplo, `PacienteAdulto` o `PacientePediatrico`). Esto permite que el comportamiento de los métodos varíe según el tipo específico de objeto.
+    - Herencia: Los métodos y atributos de la clase abstracta `Paciente` son heredados por sus clases hijas, como `PacienteAdulto` y `PacientePediatrico`. Esto permite la reutilización de código y facilita la extensión de la funcionalidad.
+    - Encapsulamiento: Se asegura la protección de los atributos de los objetos mediante el uso de getters y setters. Los datos de los pacientes no son accesibles directamente desde fuera de la clase, sino a través de métodos que permiten controlarlos y validarlos antes de modificarlos.
+    - Abstracción: El método `toString()` es un ejemplo de abstracción. La clase `Main` puede llamar a este método para obtener una representación del objeto sin conocer cómo se genera internamente, solo viendo el resultado final.
  */
 
 public class Main {
